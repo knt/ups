0fb5e709926d023301dee827ec2c17c49e33ccfc
Nichole Treadway <kntreadway@gmail.com>
2013-01-08 21:01:01 -0500

    Add package; update ship_to, shipper, address to generate necessary xml

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # UPS - a gem to generate UPS shipping labels
 
+
+
+# TODO
+Currently only the basics to get labels generated is implemented. There are a ton of optional fields and alternate shipping methods that I have not yet added in. These are indicated throughout the codebase, marked as 'TODO'.
+
 # Contributors:
 - [knt](http://github.com/knt) (Nichole Treadway)
 
--- lib/ups/address.rb
+++ lib/ups/address.rb
@@ -4,7 +4,7 @@ module Ups
 		attr_accessor :address_line1, :city, :country_code,
 
 		#Optional 
-		:address_line2, :address_line3, :state_province, :postal_code
+		:address_line2, :address_line3, :state_province, :postal_code, :is_residential
 
 		def initialize(address_line1, city, country_code, options={})
 			@address_line1 = address_line1
@@ -15,6 +15,43 @@ module Ups
 			@address_line3 = options[:address_line3]
 			@state_province = options[:state_province]
 			@postal_code = options[:postal_code]
+
+			@is_residential = options[:is_residential]
+		end
+
+
+		def to_xml
+			request = Nokogiri::XML::Builder.new do |xml|
+				xml.Address {
+					xml.AddressLine1 @address_line1
+
+					unless @address_line2.nil?
+						xml.AddressLine2 @address_line2
+					end
+
+					unless @address_line3.nil?
+						xml.AddressLine3 @address_line3
+					end
+
+					xml.City @city
+
+					unless @state_province.nil?
+						xml.StateProvinceCode @state_province
+					end
+
+					unless @postal_code.nil?
+						xml.PostalCode @postal_code
+					end
+
+					xml.CountryCode @country_code
+
+					unless @is_residential.nil?
+						xml.ResidentialAddress
+					end
+				}
+			end
+      
+      Nokogiri::XML(request.to_xml).root.to_xml
 		end
 	end
 end
